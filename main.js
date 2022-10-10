db4f9602fa3489cae45a6be21bd922586d0642e7
Innerslayer <103618774+Innerslayer@users.noreply.github.com>
2022-10-10 06:35:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function preload() {
+
+}
+
+function draw() {
+
+}
+
+function takeaphoto() {
+    save('myfilter.png');
+}
